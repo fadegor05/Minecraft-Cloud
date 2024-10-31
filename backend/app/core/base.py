ec0940ee6a67fe5fd9f2dc0bce339a393e6b4b16
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-10-31 12:58:08 +0400

    moved consts to base

---
+++ backend/app/core/base.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+SAMPLE_CONFIG = {"paths": ["mods"], "auth_token": "123"}
+BASE_PATH: Path = Path("/minecraft-cloud-data")
+INSTANCE_PATH: Path = BASE_PATH / "instance"
+CONFIG_FILE: Path = BASE_PATH / "config.json"
