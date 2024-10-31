4ebda3a9c251d171055bc7804fd05ba4f6afa061
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-10-31 12:58:29 +0400

    fixed async writer, reader

--- backend/app/core/utils.py
+++ backend/app/core/utils.py
@@ -4,11 +4,11 @@ from typing import Dict
 
 
 async def write_json(data: Dict, path: Path) -> None:
-    async with open(path, "w") as file:
+    with open(path, "w") as file:
         json.dump(data, file)
 
 
 async def read_json(path: Path) -> Dict:
-    async with open(path, "r") as file:
+    with open(path, "r") as file:
         data = json.load(file)
     return data
