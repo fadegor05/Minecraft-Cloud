9866f198a7e3119f1d751e37ac4d1da278eae16c
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-03 12:52:46 +0400

    fixed exception to fastapi

--- backend/app/api/v1/download.py
+++ backend/app/api/v1/download.py
@@ -1,5 +1,4 @@
-from http.client import HTTPException
-
+from fastapi import HTTPException
 from fastapi.responses import FileResponse
 
 from app.api.v1.router import v1_router
@@ -11,6 +10,6 @@ async def get_sync_download_file(file_path: str) -> FileResponse:
     full_path = INSTANCE_PATH / file_path
 
     if not full_path.exists() or not full_path.is_file():
-        raise HTTPException(500, "File not exists")
+        raise HTTPException(status_code=404, detail="File not exists")
 
     return FileResponse(full_path)
