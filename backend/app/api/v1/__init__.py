20b874b0e549f61257e088388934914f3fdf18d3
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-03 12:28:53 +0400

    integrated new endpoints to api

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,4 +1,5 @@
-from .router import v1_router
-
-from .ping import ping
+from .download import get_sync_download_file
 from .paths import get_paths
+from .ping import ping
+from .router import v1_router
+from .upload import post_sync_upload_file
