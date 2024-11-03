e0a0050cd973097c8e0297cc4ff15b134843c913
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-03 13:12:29 +0400

    added validated ping response

--- backend/app/api/v1/utilities/ping.py
+++ backend/app/api/v1/utilities/ping.py
@@ -1,6 +1,7 @@
 from app.api.v1.router import v1_router
+from app.schemas.ping import PingResponse
 
 
 @v1_router.get("/ping", tags=["Utilities"])
-async def ping():
-    return {"hello": "world"}
+async def ping() -> PingResponse:
+    return PingResponse(status="API is up!")
