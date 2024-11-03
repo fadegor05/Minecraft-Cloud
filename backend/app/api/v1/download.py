74315bbe73d2ad92d777f3de870e5d3da762c398
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-03 12:28:01 +0400

    added download endpoint

---
+++ backend/app/api/v1/download.py
@@ -0,0 +1,16 @@
+from http.client import HTTPException
+
+from fastapi.responses import FileResponse
+
+from app.api.v1.router import v1_router
+from app.core.base import INSTANCE_PATH
+
+
+@v1_router.get("/sync/download/{file_path:path}")
+async def get_sync_download_file(file_path: str) -> FileResponse:
+    full_path = INSTANCE_PATH / file_path
+
+    if not full_path.exists() or not full_path.is_file():
+        raise HTTPException(500, "File not exists")
+
+    return FileResponse(full_path)
