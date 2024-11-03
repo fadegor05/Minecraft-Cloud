d872ff39338e99404aa4cc2d792367fbf51b0c7e
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-03 15:24:50 +0400

    added client sync endpoint

---
+++ backend/app/api/v1/sync/client.py
@@ -0,0 +1,20 @@
+from fastapi import Depends
+
+from app.api.v1.router import v1_router
+from app.core.base import INSTANCE_PATH
+from app.core.config import Config, get_config_dependency
+from app.schemas.sync import SyncClientBody, SyncClientResponse
+from app.utils.auth import require_api_key
+from app.utils.dict import get_pydantic_diff
+from app.utils.hash import get_pydantic_hash
+from app.utils.path import get_directory_hash
+
+
+@v1_router.post("/sync/client", tags=["Sync"], dependencies=[Depends(require_api_key)])
+async def sync_client(body: SyncClientBody, config: Config = Depends(get_config_dependency)) -> SyncClientResponse:
+    # TODO: Add caching, if server_hash == client_hash
+    server_hash_tree = await get_directory_hash(INSTANCE_PATH, config.paths)
+    server_hash = await get_pydantic_hash(server_hash_tree)
+    download, delete = await get_pydantic_diff(body.client_hash_tree, server_hash_tree)
+    return SyncClientResponse(server_hash=server_hash, server_hash_tree=server_hash_tree, client_download=download,
+                              client_delete=delete)
